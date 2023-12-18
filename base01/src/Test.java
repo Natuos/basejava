18f1bf7ee3bbd64a7fd7f984b25358baac8b08e3
Natuos <Natuos@qq.com>
2023-12-18 21:34:39 +0800

    第二章

---
+++ base01/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Natuos
+ * @date : 2023-12-18 20:32
+ * @Description请使用一句输出语句，达到输入如下图形的效果
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t\t1000");
+    }
+}
